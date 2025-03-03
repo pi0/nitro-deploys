da6b0c3b503db961dffa48629fecca8d377b5301
Pooya Parsa <pooya@pi0.io>
2025-03-03 21:15:17 +0100

    update env api

--- server/routes/api/env.ts
+++ server/routes/api/env.ts
@@ -1,8 +1,9 @@
 export default eventHandler(() => {
-  return {
-    processEnv: safeObj(process.env),
+  return JSON.stringify({
+    'process.env': safeObj(process.env),
+    'process.env.TEST': process.env.TEST,
     runtimeConfig: safeObj(useRuntimeConfig()),
-  };
+  }, undefined, 2);
 });
 
 const tokenRe = /password|token|key|secret/i;
